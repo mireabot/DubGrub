3421f758f93f09c8b70a2fc68bfe9cfd5e0504a6
Mikhail Kolkov <kolkov.michael2014@yandex.ru>
2022-11-08 15:49:44 -0500

    Initial Commit

---
+++ DubGrub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubGrub
+//
+//  Created by Mikhail Kolkov on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubGrub/DubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubGrubApp.swift
+//  DubGrub
+//
+//  Created by Mikhail Kolkov on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
